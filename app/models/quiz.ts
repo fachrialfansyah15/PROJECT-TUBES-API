a0c3d0d2325475fddcc11150060102dbd5be7492
asyqarilandika <asyqarilandika10@gmail.com>
2025-10-19 16:05:59 +0800

    update migration n model

---
+++ app/models/quiz.ts
@@ -0,0 +1,50 @@
+import { BaseModel, column, belongsTo, hasMany } from '@adonisjs/lucid/orm'
+import type { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
+import User from './user.js'
+import Question from './question.js'
+import Result from './result.js'
+import UserAnswer from './user_answer.js'
+import { DateTime } from 'luxon'
+
+export default class Quiz extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare title: string
+
+  @column()
+  declare description: string | null
+
+  @column()
+  declare created_by: number
+
+  @column.dateTime({ autoCreate: true })
+  declare created_at: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updated_at: DateTime
+
+  // Relasi: quiz dimiliki oleh satu user
+  @belongsTo(() => User, {
+    foreignKey: 'created_by',
+  })
+  declare user: BelongsTo<typeof User>
+
+  // Relasi: satu quiz memiliki banyak pertanyaan
+  @hasMany(() => Question, {
+    foreignKey: 'quiz_id',
+  })
+  declare questions: HasMany<typeof Question>
+
+  // ✅ Relasi baru: satu quiz memiliki banyak hasil (results)
+  @hasMany(() => Result, {
+    foreignKey: 'quiz_id',
+  })
+  declare results: HasMany<typeof Result>
+
+  @hasMany(() => UserAnswer, {
+    foreignKey: 'quiz_id',
+  })
+  declare userAnswers: HasMany<typeof UserAnswer>
+}
---
+++ app/models/user.ts
@@ -0,0 +1,45 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+import Quiz from './quiz.js'
+import Result from './result.js'
+import UserAnswer from './user_answer.js'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare email: string
+
+  @column({ serializeAs: null })
+  declare password: string
+
+  @column()
+  declare role: 'admin' | 'user'
+
+  @column.dateTime({ autoCreate: true })
+  declare created_at: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updated_at: DateTime
+
+  // Relasi: 1 User bisa punya banyak Quiz
+  @hasMany(() => Quiz, {
+    foreignKey: 'created_by',
+  })
+  declare quizzes: HasMany<typeof Quiz>
+
+  @hasMany(() => Result, {
+    foreignKey: 'user_id',
+  })
+  declare results: HasMany<typeof Result>
+
+  @hasMany(() => UserAnswer, {
+    foreignKey: 'user_id',
+  })
+  declare userAnswers: HasMany<typeof UserAnswer>
+}
